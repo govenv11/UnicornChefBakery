021fcf37c8b01cf0e13da14a03837a3f8ad271d1
govenv11 <devangovender06@gmail.com>
2024-07-09 16:00:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+/* SLIDER JS */
+
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+
+    prevArrow:".site-slider.prev",
+    nextArrow: ".site-slider.next",
+    slidesToShow: 5,
+    slidesToScroll: 20,
+    autoplaySpeed: 1000
+});
+
+
+
